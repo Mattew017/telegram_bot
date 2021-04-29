0d05a287b7a2237be0a9a365778b19ed952fbacd
Mattew017 <zaelapporagrance@gmail.com>
2021-04-29 17:59:13 +0500

    some unworking shit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telethon import TelegramClient
-from telethon import sync, events
-import  time
+import requests
+from bs4 import BeautifulSoup
+import time
 
 api_id = 5691675
 api_hash = 'e45f7197100f1099c06ffd130ea028b4'
@@ -9,33 +10,35 @@ client = TelegramClient('anon', api_id, api_hash)
 
 client.start()
 
-dlgs = client.iter_dialogs()
-
-for dlg in dlgs:
-    if dlg.title == 'DOGE Click Bot':
-        tegmo = dlg
-
-msgs = client.iter_messages(tegmo, limit=1)
 
 
-def send_msg(bot_name, msg):
-    client.send_message(bot_name, msg)
+
+def get_name_from_link(link):
+    req = requests.get(link)
+    soup = BeautifulSoup(req.content, "html.parser")
+    return soup
 
 
-def get_last_msg(bot_name):
-    last_msg = next(client.iter_messages(bot_name, limit=1))
-    return last_msg
+def get_name_by_url(url):
+    pass
 
 
-def forward_msg(bot_name, msg):
-    client.forward_messages(bot_name, msg)
+def send_msg(name, msg):
+    client.send_message(name, msg)
 
 
+main_bot = "Dogecoin_click_bot"
 name = 'claim_free_bitcoin_bot'
+
+
 while True:
-    send_msg(name, "/start")
-    time.sleep(0.5)
-    last_bot_msg = get_last_msg(name)
-    print(last_bot_msg)
-    forward_msg('Dogecoin_click_bot', last_bot_msg)
-    time.sleep(15)
+    send_msg(main_bot, "/bots")
+    time.sleep(5)
+    messages = next(client.iter_messages(main_bot, limit=1))
+    url = messages.reply_markup.rows[0].buttons[0].url
+    print(url)
+    soup = get_name_from_link(url)
+    time.sleep(10)
+
+
+
